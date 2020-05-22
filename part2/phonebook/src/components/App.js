9c31aabdb7792771b4adaae9725df82bd1854a9a
Khandker <khandker.islam46@gmail.com>
2020-05-22 17:41:56 -0400

    2.7 phonebook step2

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,33 +1,46 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas' }
+        {name: 'Arto Hellas',id:1 }
     ])
-    const [newName, setNewname ] = useState('')
+    const [newName, setNewName ] = useState('')
 
-    useEffect(() => {
-        console.log('effect');
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response => {
-                console.log(response)
-            })
-    },[])
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName,
+            id: persons.length + 1
+        }
+
+        persons.some(person => person.name === personObject.name) ? alert(`${personObject.name} already exists in the phonebook`):setPersons(persons.concat(personObject))
+        setNewName('')
+    }
+
+    const handleNameChange = (event) => {
+        const input = event.target.value
+        setNewName(input);
+    }
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={addPerson}>
                 <div>
-                    name: <input />
+                    name:
+                    <input
+                        value={newName}
+                        onChange={handleNameChange}
+                    />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
+            {persons.map((person) =>
+                <p key={person.id}>{person.name}</p>
+            )}
         </div>
     )
 }
